c25d409f473bcd2e27b84f93a7501442f8488fee
Olha Teplova <olitieplove@gmail.com>
2023-12-16 23:02:34 +0200

    feat: implement hanoi-tower solution

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let sortedArr = arr.filter(el => el !== -1).sort((a,b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    (arr[i] === -1) ? sortedArr.splice(i, 0, -1) : sortedArr;
+  }
+  return sortedArr;
 }
 
 module.exports = {
